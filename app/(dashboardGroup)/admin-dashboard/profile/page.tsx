36f62985869f7e11c8cd454a7863a5b3ff4e57e4
Fahim <ashfahim18@gmail.com>
2026-09-08 01:24:48 +0600

    Admin profile completed

--- app/(dashboardGroup)/admin-dashboard/profile/page.tsx
+++ app/(dashboardGroup)/admin-dashboard/profile/page.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import SkeletonLoader from "@/app/(publicGroup)/_components/Skeleton";
+import React, { Suspense } from "react";
+import ProfileLoad from "../../_shared_components/ProfileLoad";
 
-const AdminProfile = () => {
+
+const AdminProfilePage = async () => {
+
+
     return (
-        <div>
-            Admin Profile
+        <div className="min-h-screen bg-muted/30 p-4 sm:p-6 lg:p-8">
+            <Suspense fallback={<SkeletonLoader></SkeletonLoader>}>
+                <ProfileLoad></ProfileLoad>
+            </Suspense>
         </div>
     );
 };
 
-export default AdminProfile;
+export default AdminProfilePage;
